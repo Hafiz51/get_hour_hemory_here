8ac5e881623836d7e88a9205fbed45b104218e73
Hafijul <123forvicstudy@gmail.com>
2022-06-14 01:15:24 +0600

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/adnanimageqr/';
+$config['base_url'] = 'http://localhost:40/get_hour_hemory_here/';
 
 /*
 |--------------------------------------------------------------------------
